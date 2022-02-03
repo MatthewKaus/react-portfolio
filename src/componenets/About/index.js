e5665e99d494bb03005a8e2dd73d2cf857eada38
Matthew Kaus <matthewkausfox@gmail.com>
2022-02-03 04:14:21 -0700

    initialized starting folders

---
+++ src/componenets/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section>
+            <h1>About me</h1>
+            <p>I am from a small town Gallup, New Mexico</p>
+        </section>
+    )
+}
+
+export default About;
---
+++ src/componenets/Nav/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Nav() {
+    return(
+        <header>
+            <h1>Matthew</h1>
+            <nav>
+                <ul>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Portfolio
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        Resume
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
