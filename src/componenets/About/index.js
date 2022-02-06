9879b9f8f68711f5e5fbc4aa348a89cbb4ecac6b
Matthew Kaus <matthewkausfox@gmail.com>
2022-02-05 22:48:45 -0700

    updated about me

--- src/componenets/About/index.js
+++ src/componenets/About/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 function About() {
     return(
         <section className='about'>
-            <h1>About me</h1>
-            <p>I am from small town Gallup, New Mexico and a graduate in Full Stack Web Development from the University of Arizona.
+            <h1 className='about-text'>About me</h1>
+            <p className='about-text'>I am from small town Gallup, New Mexico and a graduate in Full Stack Web Development from the University of Arizona.
                 I am specialized in the MERN stack
 
             </p>
