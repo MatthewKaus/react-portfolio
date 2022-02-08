3ffb27e30f3d17f2216fcf7bc95258b923638044
Matthew Kaus <matthewkausfox@gmail.com>
2022-02-08 15:26:16 -0700

    removed target blank" "

--- src/componenets/Footer/index.js
+++ src/componenets/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = ({ footerLinks }) => {
 
     const links = footerLinks.links.map(links => {
-        return <li key={links.name}><a href={links.url} target='_blank'><i className={links.className} /></a></li>
+        return <li key={links.name}><a href={links.url}><i className={links.className} /></a></li>
     })
 
     return (
